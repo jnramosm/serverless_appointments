7fa2bb5c52842d02459e40dab354e072cb8b7209
jnramosm <jnramos@uc.cl>
2020-08-13 13:16:10 -0400

    correction slots order

--- src/models/settings.js
+++ src/models/settings.js
@@ -223,7 +223,7 @@ const slots = async (data = {}, client, google, cb) => {
     var date = moment(data.day);
     var day = date.day();
     //In production move sat to the beggining, in development move it to the end
-    const dayOfWeek = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+    const dayOfWeek = ["sat", "sun", "mon", "tue", "wed", "thu", "fri"];
     db.collection("users")
       .find({ email: data.email })
       .toArray()
